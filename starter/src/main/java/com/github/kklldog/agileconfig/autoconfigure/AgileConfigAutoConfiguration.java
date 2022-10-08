5feeff23d53856899207308c9226ccc257843cba
warriorg <warriorg@users.noreply.github.com>
2022-10-08 17:15:36 +0800

    create config bean

--- starter/src/main/java/com/github/kklldog/agileconfig/autoconfigure/AgileConfigAutoConfiguration.java
+++ starter/src/main/java/com/github/kklldog/agileconfig/autoconfigure/AgileConfigAutoConfiguration.java
@@ -1,5 +1,11 @@
 package com.github.kklldog.agileconfig.autoconfigure;
 
+import com.github.kklldog.agileconfig.ConfigClient;
+import com.github.kklldog.agileconfig.IConfigClient;
+import com.github.kklldog.agileconfig.Options;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,4 +16,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(value = "com.github.kklldog.agileconfig.**")
 public class AgileConfigAutoConfiguration {
+
+    @Value("${agile.config.node}")
+    private String node;
+    @Value("${agile.config.appId:}")
+    private String appId;
+    @Value("${agile.config.secret:}")
+    private String secret;
+    @Value("${agile.config.env:dev}")
+    private String env;
+
+
+    @Bean
+    @ConditionalOnMissingBean(name = "agileConfigClient")
+    public IConfigClient configClient() {
+        return new ConfigClient(new Options(node, appId, secret, env));
+    }
 }
